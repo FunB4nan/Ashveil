e49ee7211ab9a9b9222840d7aaae1a14e30237ed
FunB4nan <funb4nan@gmail.com>
2025-10-04 00:53:18 +0300

    talo test

--- scripts/Autoload/Global.gd
+++ scripts/Autoload/Global.gd
@@ -6,6 +6,10 @@ var musicValume = 100
 var sfxValume = 100
 var crtOn = true
 
-var time = 0
-
 const languages = ["english","русский"]
+
+func _ready() -> void:
+	await get_tree().create_timer(1).timeout
+	await Talo.players.identify("dev", "me")
+	Talo.events.track("New Entry", {"Level" : "0"})
+	print("success")
